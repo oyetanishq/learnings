f3a8f42e6fec46f9acfcaa990ae06ab19977388e
Tanishq Singh <hi@tanishqsingh.com>
2025-06-13 13:17:02 +0530

    test: check production deployment

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-    return <h1 className="text-xs text-red-800">home</h1>;
+    return (
+        <div className="min-h-dvh flex justify-center items-center">
+            <h1 className="text-xs text-gray-800 underline underline-offset-4 duration-300 hover:-skew-6 cursor-pointer">
+                home
+            </h1>
+        </div>
+    );
 }
